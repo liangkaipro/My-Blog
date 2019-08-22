abfe97a247176763bff272d88d3197d5c7a250e5
liangkaipro <641919556@qq.com>
2019-08-22 16:11:00 +0800

    add Model

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 # encoding: utf-8
 from flask_script import Manager, Server
-from main import app, db, User
+from main import app, db, User, Post
+from flask_migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("server", Server())
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Post=Post)
 
 
 if __name__ == '__main__':
